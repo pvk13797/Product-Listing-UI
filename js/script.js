4bf631c1098c2879afd1463b3c1c5a8f7f6b49be
prasanna v <prasanna.vasudevan@siamcomputing.com>
2022-11-08 19:07:58 +0530

    updated live search

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+    // Search
+    $('#search').keyup(function(){
+        var filter = $(this).val();
+        var count = 0;
+        $('.cols').each(function(){
+            if($(this).text().search(new RegExp(filter, 'i')) < 0){
+                $(this).fadeOut();
+            } else {
+                $(this).show();
+                count++;
+            }
+        })
+    });
     $('.cols').slice(0, 3).show();
-    console.log('jQuery Started!!!');
     $('.view_more a').click(function(e){
         e.preventDefault();
         $('.cols:hidden').slice(0, 3).show();
